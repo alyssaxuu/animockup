29581c9e7fbd9e741f0086e549d327d2bf68a101
Alyssa X <hi@alyssax.com>
2020-11-02 10:06:02 +0100

    Use FFMPEG through local file

--- convert.js
+++ convert.js
@@ -1,8 +1,4 @@
-var inner = document.getElementById("convert");
-var workerPath = 'https://archive.org/download/ffmpeg_asm/ffmpeg_asm.js';
-if (document.domain == 'localhost') {
-    workerPath = location.href.replace(location.href.split('/').pop(), '') + 'ffmpeg_asm.js';
-}
+var workerPath = './ffmpeg.js';
 
 function processInWebWorker() {
     var blob = URL.createObjectURL(new Blob(['importScripts("' + workerPath + '");var now = Date.now;function print(text) {postMessage({"type" : "stdout","data" : text});};onmessage = function(event) {var message = event.data;if (message.type === "command") {var Module = {print: print,printErr: print,files: message.files || [],arguments: message.arguments || [],TOTAL_MEMORY: message.TOTAL_MEMORY||536870912  || false};postMessage({"type" : "start","data" : Module.arguments.join(" ")});postMessage({"type" : "stdout","data" : "Received command: " +Module.arguments.join(" ") +((Module.TOTAL_MEMORY ) ? ".  Processing with " + Module.TOTAL_MEMORY + " bits." : "")});var time = now();var result = ffmpeg_run(Module);var totalTime = now() - time;postMessage({"type" : "stdout","data" : "Finished processing (took " + totalTime + "ms)"});postMessage({"type" : "done","data" : result,"time" : totalTime});}};postMessage({"type" : "ready"});'], {
